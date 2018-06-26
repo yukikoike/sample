171a40b4ee8bdf1277ce96609845559427a7d36e
Yuki Koike <yuki.koike1111@gmail.com>
2018-06-26 14:05:36 +0900

    6/26朝2

--- src/main/java/co/jp/yuki/koike/sample/SampleApplication.java
+++ src/main/java/co/jp/yuki/koike/sample/SampleApplication.java
@@ -3,10 +3,18 @@ package co.jp.yuki.koike.sample;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * エントリポイントクラス
+ */
 @SpringBootApplication
 public class SampleApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SampleApplication.class, args);
-	}
+    /**
+     * サンプルサプリケーションを起動する
+     *
+     * @param args コマンドライン引数
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(SampleApplication.class, args);
+    }
 }
