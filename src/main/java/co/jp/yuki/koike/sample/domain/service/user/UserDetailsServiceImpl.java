5ceb8201912fe8457f27913334efa45c7c340945
Yuki Koike <yuki.koike1111@gmail.com>
2018-06-27 10:32:33 +0900

    6/27朝

--- src/main/java/co/jp/yuki/koike/sample/domain/service/user/UserDetailsServiceImpl.java
+++ src/main/java/co/jp/yuki/koike/sample/domain/service/user/UserDetailsServiceImpl.java
@@ -2,6 +2,8 @@ package co.jp.yuki.koike.sample.domain.service.user;
 
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -15,21 +17,28 @@ import co.jp.yuki.koike.sample.domain.repository.user.UserRepository;
  */
 public class UserDetailsServiceImpl implements UserDetailsService {
 
-	/**
-	 * UserRepository
-	 */
-	@Autowired
-	UserRepository userRepository;
+    /**
+     * Logger
+     */
+    private final static Logger logger =
+            LoggerFactory.getLogger("co.jp.yuki.koike.sample.app.controller.UserDetailsServiceImpl");
 
-	/*
-	 * (非 Javadoc)
-	 * @see org.springframework.security.core.userdetails.UserDetailsService#loadUserByUsername(java.lang.String)
-	 */
-	@Override
-	public UserDetails loadUserByUsername(String username)
-	        throws UsernameNotFoundException {
-	    User user = Optional.ofNullable(userRepository.findOne(username))
-	            .orElseThrow(() -> new UsernameNotFoundException(username + " is not found."));
-	    return new UserDetailsImpl(user);
-	}
+    /**
+     * UserRepository
+     */
+    @Autowired
+    UserRepository userRepository;
+
+    /*
+     * (非 Javadoc)
+     * @see org.springframework.security.core.userdetails.UserDetailsService#loadUserByUsername(java.lang.String)
+     */
+    @Override
+    public UserDetails loadUserByUsername(String username)
+            throws UsernameNotFoundException {
+        User user = Optional.ofNullable(userRepository.findOne(username))
+                .orElseThrow(() -> new UsernameNotFoundException(username + " is not found."));
+        logger.debug("userId = " + user.getUserId() + "%n" + "password = " + user.getPassword());
+        return new UserDetailsImpl(user);
+    }
 }
